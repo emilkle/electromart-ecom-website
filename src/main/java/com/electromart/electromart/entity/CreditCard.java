d654b4da77c67cfc7f393f34cb0757c8efa8bb89
Mariusrd <mariusrd@stud.ntnu.no>
2024-05-01 17:40:45 +0200

    changed table annotation for Brand and User. Changed Payment to abstract class, and CreditCard now extend Payment. May need to update annotations for subclass

--- src/main/java/com/electromart/electromart/entity/CreditCard.java
+++ src/main/java/com/electromart/electromart/entity/CreditCard.java
@@ -2,22 +2,30 @@ package com.electromart.electromart.entity;
 
 import jakarta.persistence.*;
 
-import java.io.Serializable;
-
-/*
 @Entity
 @Table(name="creditcard")
-public class CreditCard implements Payment {
+public class CreditCard extends Payment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long creditCardFlag;
 
-    String thirdPartService;
+    private String thirdPartService;
 
-    public CreditCard () {}
+    public CreditCard() {}
 
-    public CreditCard (long creditCardFlag, String thirdPartService) {
+    public CreditCard(long creditCardFlag, String thirdPartService) {
+        super();
         this.creditCardFlag = creditCardFlag;
         this.thirdPartService = thirdPartService;
     }
-}*/
+
+    // Getter and setter for thirdPartService
+    public String getThirdPartService() {
+        return thirdPartService;
+    }
+
+    public void setThirdPartService(String thirdPartService) {
+        this.thirdPartService = thirdPartService;
+    }
+}
--- src/main/java/com/electromart/electromart/entity/Payment.java
+++ src/main/java/com/electromart/electromart/entity/Payment.java
@@ -2,21 +2,68 @@ package com.electromart.electromart.entity;
 
 import jakarta.persistence.*;
 
-/*
 @Entity
 @Table(name="payment")
-public interface Payment {
+abstract class Payment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long paymentID;
+    private long paymentID;
 
     @JoinColumn(name="order_id")
     @OneToOne
-    Order orderID;
+    private Order orderID;
 
-    String paymentType;
-    int paymentAmount;
-    String paymentDate;
-    String paymentStatus;
+    private String paymentType;
+    private int paymentAmount;
+    private String paymentDate;
+    private String paymentStatus;
 
-}*/
+    public long getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(long paymentID) {
+        this.paymentID = paymentID;
+    }
+
+    public Order getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(Order orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public int getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(int paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+}
