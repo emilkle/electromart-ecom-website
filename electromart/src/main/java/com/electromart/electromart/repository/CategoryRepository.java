ad4666fc457f5427ba7424fc2932cf570f6c9873
Emil <emilkle@stud.ntnu.no>
2024-05-01 16:05:08 +0200

    Removed old elektromart directory.

--- electromart/src/main/java/com/electromart/electromart/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.electromart.electromart.repository;
-
-import com.electromart.electromart.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
--- electromart/src/main/java/com/electromart/electromart/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.electromart.electromart.repository;
-
-import com.electromart.electromart.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
