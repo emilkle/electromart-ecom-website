c2fdae096728c82632e5978481b35099e3f9c821
Emil <emilkle@stud.ntnu.no>
2024-05-02 16:27:04 +0200

    Added GetMapping for getting a brand name by its id.

--- src/main/java/com/electromart/electromart/service/BrandService.java
+++ src/main/java/com/electromart/electromart/service/BrandService.java
@@ -2,6 +2,7 @@ package com.electromart.electromart.service;
 
 import com.electromart.electromart.entity.Brand;
 import com.electromart.electromart.repository.BrandRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,14 @@ public class BrandService {
     public List<Brand> getAllBrands() {
         return brandRepository.findAll();
     }
+
+    public Optional<String> getNameByID(Long id){
+        Optional<Brand> brandParam = getAllBrands()
+            .stream()
+            .filter(brand -> brand.getBrandId().equals(id))
+            .findFirst();
+        return brandParam.map(Brand::getName);
+    }
 }
+
 
