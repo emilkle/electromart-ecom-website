e8a1ec7bac77f56c048ca0e2aa8693bd687bcadd
Thomas Andreassen <thoandr@stud.ntnu.no>
2024-05-03 07:08:51 +0200

    Added shoppingcart logic, check if item already in cart, added logic functions to cartItem

--- src/main/java/com/electromart/electromart/service/ShoppingCartService.java
+++ src/main/java/com/electromart/electromart/service/ShoppingCartService.java
@@ -4,14 +4,16 @@ import com.electromart.electromart.entity.CartItem;
 import com.electromart.electromart.entity.Product;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShoppingCartService {
 
-
+//
     private final ArrayList<CartItem> shoppingCart = new ArrayList<>();
 
     // assign to inventory service?
-    private InventoryService is;
+    private final InventoryService is = new InventoryService();
 
     public ShoppingCartService () {
 
@@ -22,24 +24,42 @@ public class ShoppingCartService {
      *
      * @param product Product to add to shopping cart
      * @param amount amount of product to add to shopping cart
+     *
      */
 
     public boolean addProductToCart(Product product, int amount) {
         boolean addToCart =  false;
         long productID = product.getProductId();
         try {
+            //Check if productID, amount is valid
             if (is.checkInventoryQuantity(productID, amount)){
-                CartItem cartItem = new CartItem(product,amount);
-                shoppingCart.add(cartItem);
-                addToCart = true;
+                //Check if item is in cart
+                if (isProductInCart(product)){
+                    int indexArray = 0;
+                    for (int index = 0; index < shoppingCart.size(); index++) {
+                        if (shoppingCart.get(index).getCartItemID() == product.getProductId()) {
+                            indexArray = index;
+                        }
+                    this.shoppingCart.get(indexArray).setCartAmount(amount);
+                    }
+
+                }
+                else    {
+                    CartItem cartItem = new CartItem(product,amount);
+                    shoppingCart.add(cartItem);
+                    addToCart = true;
+                }
             }
         } catch (Exception e) {
             System.out.println("Wrong amount added to cart.");
         }
         return addToCart;
 
-
     }
 
+    private boolean isProductInCart(Product product) {
+        return  shoppingCart.stream().anyMatch(e -> e.getCartItemID() == product.getProductId());
+
+    }
 
 }
