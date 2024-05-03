d320327f8ed3c735220c2ecf516876fec9b425f3
Emil <emilkle@stud.ntnu.no>
2024-05-03 22:52:27 +0200

    Added HTTP GET mapping to fetch brand description from brand name.

--- src/main/java/com/electromart/electromart/controller/BrandController.java
+++ src/main/java/com/electromart/electromart/controller/BrandController.java
@@ -1,8 +1,8 @@
 package com.electromart.electromart.controller;
 
 import com.electromart.electromart.entity.Brand;
-import com.electromart.electromart.entity.Brand;
 import com.electromart.electromart.service.BrandService;
+import java.util.Objects;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * The type Brand controller.
+ */
 @RestController
 @RequestMapping("/brand")
 public class BrandController {
@@ -18,22 +21,56 @@ public class BrandController {
     @Autowired
     private BrandService brandService;
 
+    /**
+     * Fetch all brands list.
+     *
+     * @return the list
+     */
     @GetMapping({"", "/"})
     public List<Brand> fetchAllBrands() {
         return brandService.getAllBrands();
     }
 
+    /**
+     * Fetch brand name response entity.
+     *
+     * @param id the id
+     * @return the response entity
+     */
     @GetMapping("/brand_id={id}")
-    public ResponseEntity<String> fetchBrandName(@PathVariable("id") Long id) {
-        if (id != null) {
-            Optional<String> brandNameParam = brandService.getNameByID(id);
+    public ResponseEntity<String> fetchBrandName(@PathVariable(value = "id", required = false)
+                                                     Long id) {
+        Optional<String> brandNameParam = brandService.getNameFromID(id);
+        if (id != null && id > 0) {
             return brandNameParam.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("The brand with id " + id + " Was not found"));
         }
         else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("No id was specified.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("No valid id was specified.");
+        }
+    }
+
+    /**
+     * Fetch brand description response entity.
+     *
+     * @param name the name
+     * @return the response entity
+     */
+    @GetMapping("/name={name}")
+    public ResponseEntity<String> fetchBrandDescription(@PathVariable(
+        value = "name", required = false) String name) {
+        Optional<String> brandNameParam = brandService.getDescriptionFromName(name);
+        if (!name.isEmpty()) {
+            return brandNameParam.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("The brand with name " + name + " Was not found"));
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("No valid brand name was specified. To add a new brand " +
+                    "use the HTTP POST method.");
         }
     }
 }
--- src/main/java/com/electromart/electromart/service/BrandService.java
+++ src/main/java/com/electromart/electromart/service/BrandService.java
@@ -8,22 +8,49 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * The type Brand service.
+ */
 @Service
 public class BrandService {
 
     @Autowired
     private BrandRepository brandRepository;
 
+    /**
+     * Gets all brands.
+     *
+     * @return the all brands
+     */
     public List<Brand> getAllBrands() {
         return brandRepository.findAll();
     }
 
-    public Optional<String> getNameByID(Long id){
+    /**
+     * Get name from id optional.
+     *
+     * @param id the id
+     * @return the optional
+     */
+    public Optional<String> getNameFromID(Long id){
         Optional<Brand> brandParam = getAllBrands()
             .stream()
             .filter(brand -> brand.getBrandId().equals(id))
             .findFirst();
         return brandParam.map(Brand::getName);
+    }
+
+    /**
+     * Get description from name optional.
+     *
+     * @param name the name
+     * @return the optional
+     */
+    public Optional<String> getDescriptionFromName(String name){
+        Optional<Brand> brandParam = getAllBrands()
+            .stream().filter(brand -> brand.getName().equals(name))
+            .findFirst();
+        return brandParam.map(Brand::getDescription);
     }
 }
 
