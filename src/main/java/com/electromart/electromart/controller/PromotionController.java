490e121e4e540eaf5cbc0ac2088d9747134d819d
Emil <emilkle@stud.ntnu.no>
2024-05-06 22:20:07 +0200

    Added all HTTP GET methods for the PromotionController class and added more service methods in the PromotionService class.

--- src/main/java/com/electromart/electromart/controller/PromotionController.java
+++ src/main/java/com/electromart/electromart/controller/PromotionController.java
@@ -1,22 +1,61 @@
 package com.electromart.electromart.controller;
 
+import com.electromart.electromart.dto.BrandDTO;
+import com.electromart.electromart.dto.CategoryDTO;
+import com.electromart.electromart.dto.PromotionDTO;
 import com.electromart.electromart.entity.Promotion;
+import com.electromart.electromart.service.CategoryService;
 import com.electromart.electromart.service.ProductReviewService;
 import com.electromart.electromart.service.PromotionService;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/promotion")
 public class PromotionController {
 
-    @Autowired
-    private PromotionService promotionService;
+    private final PromotionService promotionService;
 
-    @GetMapping("")
-    public List<Promotion> fetchAllPromotions() {
-        return promotionService.getAllPromotions();
+    public PromotionController(PromotionService promotionService) {
+        this.promotionService = promotionService;
+    }
+
+    /**
+     * Handles GET requests for returning all the promotions present in the database.
+     * @return A list with all the promotions in the database.
+     * @throws ResponseStatusException with HttpStatus.NOT_FOUND if no promotions are found.
+     */
+    @GetMapping({"", "/"})
+    public List<PromotionDTO> fetchAllPromotions() {
+        List<PromotionDTO> promotions = promotionService.getAllPromotions();
+        if (promotions.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No promotions found");
+        } else {
+            return promotions;
+        }
+    }
+
+    /**
+     * Gets promotion by id.
+     *
+     * @param id the id
+     * @return the promotion by id
+     */
+    @GetMapping("/promotion_id={id}")
+    public ResponseEntity<?> getPromotionById(@PathVariable Long id) {
+        Optional<PromotionDTO> optionalPromotion = promotionService.getPromotionById(id);
+        if (optionalPromotion.isPresent()) {
+            return new ResponseEntity<>(optionalPromotion.get(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Promotion with ID: '" + id + "' not found");
+        }
     }
 }
--- src/main/java/com/electromart/electromart/service/PromotionService.java
+++ src/main/java/com/electromart/electromart/service/PromotionService.java
@@ -1,40 +1,112 @@
 package com.electromart.electromart.service;
 
+import com.electromart.electromart.dto.PromotionDTO;
 import com.electromart.electromart.entity.Product;
 import com.electromart.electromart.entity.Promotion;
-import com.electromart.electromart.repository.InventoryRepository;
 import com.electromart.electromart.repository.ProductRepository;
 import com.electromart.electromart.repository.PromotionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.Date;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class PromotionService {
 
-    @Autowired
-    private PromotionRepository promotionRepository;
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final PromotionRepository promotionRepository;
+    private final ProductRepository productRepository;
 
-    public List<Promotion> getAllPromotions () {
-        return promotionRepository.findAll();
+    public PromotionService(PromotionRepository promotionRepository,
+                            ProductRepository productRepository) {
+        this.promotionRepository = promotionRepository;
+        this.productRepository = productRepository;
     }
 
     /**
-     * This metohd calculates the price discount for a promotion
+     * Fetches all promotions that is stored in the database.
+     *
+     * @return a list of promotionDTO objects corresponding to all
+     * the promotions that are stored in the database.
+     */
+    public List<PromotionDTO> getAllPromotions() {
+        // Fetches all the promotions and store them in a list.
+        List<Promotion> promotions = promotionRepository.findAll();
+        // Goes through the list of promotions and converts each promotion object to promotionDTO
+        // objects. Then it collect and return all the converted categories in a list.
+        return promotions.stream()
+            .map(this::convertToDTO)
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Fetches a specific promotionDTO based on the promotion ID.
+     *
+     * @param id The promotion ID of the desired promotion.
+     * @return A promotionDTO that matches the specified promotion ID,
+     *         or an empty optional if no promotion with the specified ID was found.
+     */
+    public Optional<PromotionDTO> getPromotionById(Long id) {
+        Optional<Promotion> optionalPromotion = promotionRepository.findById(id);
+        return optionalPromotion.map(this::convertToDTO);
+    }
+
+    /**
+     * Converts a Promotion entity object into a PromotionDTO data transfer object.
+     * Copies the properties from the Promotion entity to the PromotionDTO.
+     * Also sets the productId manually for the PromotionDTO.
+     *
+     * @param promotion The Promotion entity object to convert.
+     * @return The converted PromotionDTO object.
+     */
+    private PromotionDTO convertToDTO(Promotion promotion) {
+        // Create new PromotionDTO object to store the converted promotion object
+        PromotionDTO promotionDTO = new PromotionDTO();
+        // Using BeanUtils library for copying the values in the promotion to the promotionDTO.
+        BeanUtils.copyProperties(promotion, promotionDTO);
+        // Set fields manually for the promotionDTO.
+        promotionDTO.setProductID(promotion.getProduct().getProductId());
+        return promotionDTO;
+    }
+
+    /**
+     * Converts a PromotionDTO data transfer object into a Promotion entity object.
+     * Copies the properties from the PromotionDTO to the Promotion entity.
+     *
+     * @param promotionDTO The PromotionDTO object to convert.
+     * @return The converted Promotion entity object.
+     */
+    private Promotion convertToEntity(PromotionDTO promotionDTO) {
+        Promotion promotion = new Promotion();
+        BeanUtils.copyProperties(promotionDTO, promotion);
+        Product product = new Product();
+        product.setProductId(promotionDTO.getProductID());
+        promotion.setProduct(product);
+        promotion.getProduct().setProductId(promotionDTO.getProductID());
+        return promotion;
+    }
+
+    /**
+     * This method calculates the price discount for a promotion
+     *
+     * @param product   the product
+     * @param promotion the promotion
+     * @return the float
      */
     public float calculateDiscountPrice (Product product, Promotion promotion) {
         float originalPrice = product.getPrice();
-        float discountvalue = promotion.getDiscountValue();
+        float discountValue = promotion.getDiscountValue();
 
         if (promotion.getDiscountType().equals("percentage")) {
-            return originalPrice * (1-discountvalue/100);
+            return originalPrice * (1-discountValue/100);
         } else if (Objects.equals(promotion.getDiscountType(), "fixed")) {
-            return originalPrice - discountvalue;
+            return originalPrice - discountValue;
         } else{
             throw new IllegalArgumentException("Invalid discount type");
         }
@@ -50,8 +122,11 @@ public class PromotionService {
      * @param startDate promotion start date
      * @param endDate promotion end date
      */
-    public void startPromotionAndUpdatePrice (Product productID, String description, String discountType, int discountValue, String startDate, String endDate) {
-        Promotion promotion = new Promotion(productID, description, discountType, discountValue, startDate, endDate);
+    public void startPromotionAndUpdatePrice (Product productID, String description,
+                                              String discountType, int discountValue,
+                                              Date startDate, Date endDate) {
+        Promotion promotion = new Promotion(productID, description, discountType,
+            discountValue, startDate, endDate);
         promotionRepository.save(promotion);
         float discountPrice = calculateDiscountPrice(productID,promotion);
         productID.setPrice(discountPrice);
