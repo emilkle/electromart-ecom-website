2637429c4dc8b82a361d322166f00e05a0f3d2c0
Emil <emilkle@stud.ntnu.no>
2024-05-07 16:47:52 +0200

    Added HTTP GET methods to the ProductReviewController and GET request services to the ProductReviewService class.

--- src/main/java/com/electromart/electromart/controller/ProductReviewController.java
+++ src/main/java/com/electromart/electromart/controller/ProductReviewController.java
@@ -1,21 +1,69 @@
 package com.electromart.electromart.controller;
 
+import com.electromart.electromart.dto.ProductReviewDTO;
 import com.electromart.electromart.entity.ProductReview;
 import com.electromart.electromart.service.ProductReviewService;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * Controller class responsible for handling product review-related HTTP requests.
+ */
 @RestController
 @RequestMapping("/productReview")
 public class ProductReviewController {
 
-    @Autowired
-    private ProductReviewService productReviewService;
+    private final ProductReviewService productReviewService;
 
-    @GetMapping("")
-    public List<ProductReview> fetchAllProductReviews() {
-        return productReviewService.getAllProductReviews();
+    /**
+     * Constructor for ProductReviewController.
+     *
+     * @param productReviewService The service responsible for handling product
+     *                             review-related operations.
+     */
+    public ProductReviewController(ProductReviewService productReviewService) {
+        this.productReviewService = productReviewService;
+    }
+
+    /**
+     * Handles GET requests to retrieve all product reviews.
+     *
+     * @return A list of ProductReviewDTO objects representing all product reviews.
+     * @throws ResponseStatusException with HttpStatus.NOT_FOUND if no product reviews are found.
+     */
+    @GetMapping({"", "/"})
+    public List<ProductReviewDTO> fetchAllProductReviews() {
+        List<ProductReviewDTO> productReviewList = productReviewService.getAllProductReviews();
+        if (productReviewList.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No product reviews found.");
+        } else {
+            return productReviewList;
+        }
+    }
+
+    /**
+     * Handles GET requests to retrieve a specific product review by ID.
+     *
+     * @param id The ID of the product review to retrieve.
+     * @return ResponseEntity with the ProductReviewDTO object if found, or NOT_FOUND status with
+     * a message if not found.
+     */
+    @GetMapping("/review_id={id}")
+    public ResponseEntity<?> getProductReviewById(@PathVariable Long id) {
+        Optional<ProductReviewDTO> optionalProductReview =
+            productReviewService.getProductReviewById(id);
+        if (optionalProductReview.isPresent()) {
+            return new ResponseEntity<>(optionalProductReview.get(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Product review with ID: '" + id + "' not found.");
+        }
     }
 }
--- src/main/java/com/electromart/electromart/service/ProductReviewService.java
+++ src/main/java/com/electromart/electromart/service/ProductReviewService.java
@@ -1,19 +1,106 @@
 package com.electromart.electromart.service;
 
+import com.electromart.electromart.dto.ProductReviewDTO;
+import com.electromart.electromart.entity.Order;
+import com.electromart.electromart.entity.Product;
 import com.electromart.electromart.entity.ProductReview;
+import com.electromart.electromart.entity.User;
 import com.electromart.electromart.repository.ProductReviewRepository;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * Service class responsible for handling product review-related operations.
+ * Acts as an intermediary between the controller and the repository layer.
+ */
 @Service
 public class ProductReviewService {
 
-    @Autowired
-    private ProductReviewRepository productReviewRepository;
+    private final ProductReviewRepository productReviewRepository;
 
-    public List<ProductReview> getAllProductReviews() {
-        return productReviewRepository.findAll();
+    /**
+     * Constructor for ProductReviewService.
+     *
+     * @param productReviewRepository The repository for managing ProductReview entities.
+     */
+    public ProductReviewService(ProductReviewRepository productReviewRepository) {
+        this.productReviewRepository = productReviewRepository;
+    }
+
+    /**
+     * Fetches all product reviews stored in the database.
+     *
+     * @return A list of ProductReviewDTO objects corresponding to all
+     * product reviews in the database.
+     */
+    public List<ProductReviewDTO> getAllProductReviews() {
+        List<ProductReview> productReviews = productReviewRepository.findAll();
+        return productReviews.stream()
+            .map(this::convertToDTO)
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Fetches a specific product review based on the review ID.
+     *
+     * @param id The review ID of the desired product review.
+     * @return An Optional containing a ProductReviewDTO that matches the specified ID,
+     * or an empty Optional if no product review is found.
+     */
+    public Optional<ProductReviewDTO> getProductReviewById(Long id) {
+        Optional<ProductReview> optionalProductReview = productReviewRepository.findById(id);
+        return optionalProductReview.map(this::convertToDTO);
+    }
+
+    /**
+     * Converts a ProductReview entity object into a ProductReviewDTO data transfer object.
+     *
+     * @param productReview The ProductReview entity object to convert.
+     * @return The converted ProductReviewDTO object.
+     */
+    private ProductReviewDTO convertToDTO(ProductReview productReview) {
+        ProductReviewDTO productReviewDTO = new ProductReviewDTO();
+        BeanUtils.copyProperties(productReview, productReviewDTO);
+        productReviewDTO.setUserID(productReview.getUser().getUserId());
+        productReviewDTO.setProductID(productReview.getProduct().getProductId());
+        return productReviewDTO;
+    }
+
+    /**
+     * Converts a ProductReviewDTO data transfer object into a ProductReview entity object.
+     *
+     * @param productReviewDTO The ProductReviewDTO object to convert.
+     * @return The converted ProductReview entity object.
+     */
+    private ProductReview convertToEntity(ProductReviewDTO productReviewDTO) {
+        ProductReview productReview = new ProductReview();
+        BeanUtils.copyProperties(productReviewDTO, productReview);
+
+        // Add the userID from the productReviewDTO to the user.
+        User user = new User();
+        user.setUserId(productReviewDTO.getUserID());
+
+        // Set the userID of the productReview to be the user's userID.
+        productReview.setUser(user);
+        productReview.getUser().setUserId(productReviewDTO.getUserID());
+
+        // Add the productID from the productReviewDTO to the product.
+        Product product = new Product();
+        product.setProductId(productReviewDTO.getProductID());
+
+        // Set the productID of the productReview to be the product's productID.
+        productReview.setProduct(product);
+        productReview.getProduct().setProductId(productReviewDTO.getProductID());
+
+        // Return the productReview with the User and Product objects respectively containing
+        // the input userID and productID.
+        return productReview;
     }
 }
