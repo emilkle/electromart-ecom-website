0790953f1ea163489ed89103fcb28ef6acefaf4d
Emil <emilkle@stud.ntnu.no>
2024-05-09 17:31:48 +0200

    Fixed the HTTP POST and HTTP DELETE methods of the Payment endpoint, and changed the setup of the Payment entity.

--- src/main/java/com/electromart/electromart/service/PaymentService.java
+++ src/main/java/com/electromart/electromart/service/PaymentService.java
@@ -1,18 +1,22 @@
 package com.electromart.electromart.service;
 
 import com.electromart.electromart.dto.PaymentDTO;
-import com.electromart.electromart.dto.ProductReviewDTO;
-import com.electromart.electromart.entity.*;
+import com.electromart.electromart.entity.CreditCard;
+import com.electromart.electromart.entity.Cryptocurrency;
+import com.electromart.electromart.entity.Klarna;
+import com.electromart.electromart.entity.Order;
+import com.electromart.electromart.entity.Payment;
+import com.electromart.electromart.entity.Shipping;
+import com.electromart.electromart.entity.Vipps;
 import com.electromart.electromart.repository.PaymentRepository;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -29,8 +33,8 @@ public class PaymentService {
      * product reviews in the database.
      */
     public List<PaymentDTO> getAllPayments() {
-        List<Payment> productReviews = paymentRepository.findAll();
-        return productReviews.stream()
+        List<Payment> payments = paymentRepository.findAll();
+        return payments.stream()
             .map(this::convertToDTO)
             .collect(Collectors.toList());
     }
@@ -83,7 +87,6 @@ public class PaymentService {
         BeanUtils.copyProperties(payment, paymentDTO);
         paymentDTO.setPaymentID(payment.getPaymentID());
         paymentDTO.setOrderID(payment.getOrder().getOrderID());
-        paymentDTO.setPaymentMethod(payment.getPaymentMethod());
         return paymentDTO;
     }
 
@@ -97,81 +100,10 @@ public class PaymentService {
     private Payment convertToEntity(PaymentDTO paymentDTO) {
         Payment payment = new Payment();
         BeanUtils.copyProperties(paymentDTO, payment);
-
         Order order = new Order();
         order.setOrderID(paymentDTO.getOrderID());
         payment.setOrder(order);
         payment.getOrder().setOrderID(paymentDTO.getOrderID());
-
-
         return payment;
     }
-
-    /**
-     * Calculates the total payment amount from order total and shipping cost
-     * @param order the given order
-     * @param shipping the given method of shipping
-     * @return the sum of order cost and shipping cost
-     */
-    public float calculateTotalPayment (Order order, Shipping shipping) {
-        float sumOfOrder = order.getOrderTotalAmount();
-        float shippingCost = shipping.getShippingCost();
-        return sumOfOrder + shippingCost;
-    }
-
-    /**
-     * Creates a new payment and checks what kind of payment is to be made
-     * @param order corresponding order ID
-     * @param shippingID corresponding shipping ID
-     * @param paymentType method of payment
-     * @param paymentDate date of payment
-     * @param paymentStatus status of payment
-     */
- /*   public void newPayment (Order order, Shipping shippingID,  String paymentType, String paymentDate, String paymentStatus) {
-        //User clicks on button for desired payment method, given method is used as parameter in this method.
-        float totalAmount = calculateTotalPayment(order,shippingID);
-
-        switch (paymentType){
-            case "Credit card":
-                CreditCard creditCardPayment = new CreditCard();
-                creditCardPayment.setOrderID(order);
-                creditCardPayment.setPaymentAmount(totalAmount);
-                creditCardPayment.setThirdPartService("PayPal");
-                creditCardPayment.setPaymentDate(LocalDate.now().toString());
-                creditCardPayment.setPaymentStatus("Status for payment");
-                creditCardPayment.setPaymentType("credit card");
-                paymentRepository.save(creditCardPayment);
-                break;
-            case "Klarna":
-                Klarna klarnaPayment = new Klarna();
-                klarnaPayment.setOrderID(order);
-                klarnaPayment.setPaymentAmount(totalAmount);
-                klarnaPayment.setEmailAddress("EmailAddresshere");
-                klarnaPayment.setPaymentDate(LocalDate.now().toString());
-                klarnaPayment.setPaymentStatus("Status for payment");
-                klarnaPayment.setPaymentType("Klarna");
-                paymentRepository.save(klarnaPayment);
-                break;
-            case "Vipps":
-                Vipps vippsPayment = new Vipps();
-                vippsPayment.setOrderID(order);
-                vippsPayment.setPaymentAmount(totalAmount);
-                vippsPayment.setPhoneNumber(123415135);
-                vippsPayment.setPaymentDate(LocalDate.now().toString());
-                vippsPayment.setPaymentStatus("Status for payment");
-                vippsPayment.setPaymentType("Vipps");
-                paymentRepository.save(vippsPayment);
-                break;
-            case "Cryptocurrency":
-                Cryptocurrency cryptoPayment = new Cryptocurrency();
-                cryptoPayment.setOrderID(order);
-                cryptoPayment.setPaymentAmount(totalAmount);
-                cryptoPayment.setCurrencyName("Bitcoin");
-                cryptoPayment.setPaymentDate(LocalDate.now().toString());
-                cryptoPayment.setPaymentStatus("Status for payment");
-                cryptoPayment.setPaymentType("Cryptocurrency");
-                paymentRepository.save(cryptoPayment);
-                break;
-        }
-    }*/
 }
